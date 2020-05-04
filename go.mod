1cb60139af9ceeadd4ca459c2d0e3d0a6d9c286e
Katy Moe <katy@katy.moe>
2020-05-04 01:11:13 +0100

    InstallTerraform feature not currently used

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.14
 
 require (
 	github.com/davecgh/go-spew v1.1.1
-	github.com/hashicorp/go-getter v1.4.1
 	github.com/hashicorp/terraform-json v0.5.0
+	golang.org/x/text v0.3.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package tfexec
 
 import (
-	"fmt"
-	"io/ioutil"
 	"os"
 	"os/exec"
-	"path/filepath"
-	"runtime"
-
-	getter "github.com/hashicorp/go-getter"
 )
-
-const releaseHost = "https://releases.hashicorp.com"
 
 // FindTerraform attempts to find a Terraform CLI executable.
 //
@@ -31,48 +23,4 @@ func FindTerraform() string {
 		return ""
 	}
 	return p
-}
-
-func tfURL(version, osName, archName string) string {
-	return fmt.Sprintf(
-		"%s/terraform/%s/terraform_%s_%s_%s.zip",
-		releaseHost, version, version, osName, archName,
-	)
-}
-
-// InstallTerraform downloads and decompresses a Terraform CLI executable with
-// the specified version, downloaded from the HashiCorp releases page over HTTP.
-//
-// The version string must match an existing Terraform release semver version,
-// e.g. 0.12.5.
-//
-// The terraform executable is installed to a temporary folder.
-// TODO: method for cleaning up this temporary folder.
-func InstallTerraform(tfVersion string) (string, error) {
-	osName := runtime.GOOS
-	archName := runtime.GOARCH
-
-	var tfDir string
-	var err error
-
-	tfDir, err = ioutil.TempDir("", "tfexec")
-	if err != nil {
-		return "", fmt.Errorf("failed to create temp dir: %s", err)
-	}
-
-	url := tfURL(tfVersion, osName, archName)
-
-	client := getter.Client{
-		Src: url,
-		Dst: tfDir,
-
-		Mode: getter.ClientModeDir,
-	}
-
-	err = client.Get()
-	if err != nil {
-		return "", fmt.Errorf("failed to download terraform from %s: %s", url, err)
-	}
-
-	return filepath.Join(tfDir, "terraform"), nil
 }
