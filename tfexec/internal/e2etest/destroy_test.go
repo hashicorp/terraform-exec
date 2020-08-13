7f99a00d8094de36e89531c7b54a22f9c8ee78c1
Paul Tyng <paul@paultyng.net>
2020-08-12 20:09:53 -0400

    More standardization of version matrix

--- tfexec/internal/e2etest/destroy_test.go
+++ tfexec/internal/e2etest/destroy_test.go
@@ -4,25 +4,27 @@ import (
 	"context"
 	"testing"
 
+	"github.com/hashicorp/terraform-exec/tfexec"
 	"github.com/hashicorp/terraform-exec/tfexec/internal/testutil"
 )
 
 func TestDestroy(t *testing.T) {
-	tf, cleanup := setupFixture(t, testutil.Latest012, "basic")
-	defer cleanup()
+	runTest(t, []string{
+		testutil.Latest012,
+	}, "basic", func(t *testing.T, tfv string, tf *tfexec.Terraform) {
+		err := tf.Init(context.Background())
+		if err != nil {
+			t.Fatalf("error running Init in test directory: %s", err)
+		}
 
-	err := tf.Init(context.Background())
-	if err != nil {
-		t.Fatalf("error running Init in test directory: %s", err)
-	}
+		err = tf.Apply(context.Background())
+		if err != nil {
+			t.Fatalf("error running Apply: %s", err)
+		}
 
-	err = tf.Apply(context.Background())
-	if err != nil {
-		t.Fatalf("error running Apply: %s", err)
-	}
-
-	err = tf.Destroy(context.Background())
-	if err != nil {
-		t.Fatalf("error running Destroy: %s", err)
-	}
+		err = tf.Destroy(context.Background())
+		if err != nil {
+			t.Fatalf("error running Destroy: %s", err)
+		}
+	})
 }
--- tfexec/internal/e2etest/errors_test.go
+++ tfexec/internal/e2etest/errors_test.go
@@ -9,35 +9,32 @@ import (
 	"os"
 	"testing"
 
+	"github.com/hashicorp/terraform-exec/tfexec"
 	"github.com/hashicorp/terraform-exec/tfexec/internal/testutil"
 )
 
 func TestUnparsedError(t *testing.T) {
 	// This simulates an unparsed error from the Cmd.Run method (in this case file not found). This
 	// is to ensure we don't miss raising unexpected errors in addition to parsed / well known ones.
-	for _, tfv := range []string{
+	runTest(t, []string{
 		testutil.Latest011,
 		testutil.Latest012,
 		testutil.Latest013,
-	} {
-		t.Run(tfv, func(t *testing.T) {
-			tf, cleanup := setupFixture(t, tfv, "")
-			defer cleanup()
+	}, "", func(t *testing.T, tfv string, tf *tfexec.Terraform) {
 
-			// force delete the working dir to cause an os.PathError
-			err := os.RemoveAll(tf.WorkingDir())
-			if err != nil {
-				t.Fatal(err)
-			}
+		// force delete the working dir to cause an os.PathError
+		err := os.RemoveAll(tf.WorkingDir())
+		if err != nil {
+			t.Fatal(err)
+		}
 
-			err = tf.Init(context.Background())
-			if err == nil {
-				t.Fatalf("expected error running Init, none returned")
-			}
-			var e *os.PathError
-			if !errors.As(err, &e) {
-				t.Fatalf("expected os.PathError, got %T, %s", err, err)
-			}
-		})
-	}
+		err = tf.Init(context.Background())
+		if err == nil {
+			t.Fatalf("expected error running Init, none returned")
+		}
+		var e *os.PathError
+		if !errors.As(err, &e) {
+			t.Fatalf("expected os.PathError, got %T, %s", err, err)
+		}
+	})
 }
