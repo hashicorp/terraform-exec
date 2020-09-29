f3139489074b7de5f10b96200d9f6019aa8763c1
Radek Simko <radek.simko@gmail.com>
2020-09-29 12:23:01 +0100

    Make special ExitError.Error for Go <1.14

--- tfexec/errors.go
+++ tfexec/errors.go
@@ -2,7 +2,6 @@ package tfexec
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"os/exec"
 	"regexp"
@@ -215,30 +214,4 @@ type ExitError struct {
 
 func (e *ExitError) Unwrap() error {
 	return e.err
-}
-
-func (e *ExitError) Error() string {
-	var out string
-	ee, ok := e.err.(*exec.ExitError)
-	if ok {
-		out = fmt.Sprintf("%q (pid %d) exited (code %d): %s",
-			e.args,
-			ee.Pid(),
-			ee.ExitCode(),
-			ee.ProcessState.String())
-		if e.ctxErr != nil {
-			out += fmt.Sprintf("\n%s", e.ctxErr)
-		}
-	} else {
-		out = fmt.Sprintf("%q exited: %s", e.args, e.err.Error())
-		if e.ctxErr != nil && !errors.Is(e.err, e.ctxErr) {
-			out += fmt.Sprintf("\n%s", e.ctxErr)
-		}
-	}
-
-	if e.stderr != "" {
-		out += fmt.Sprintf("\nstderr: %q", e.stderr)
-	}
-
-	return out
 }
---
+++ tfexec/errors_exiterror_go112.go
@@ -0,0 +1,34 @@
+// +build go1.12 go1.13
+
+package tfexec
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func (e *ExitError) Error() string {
+	var out string
+	ee, ok := e.err.(*exec.ExitError)
+	if ok {
+		out = fmt.Sprintf("%q (pid %d) exited (code %d): %s",
+			e.args,
+			ee.Pid(),
+			ee.ExitCode(),
+			ee.ProcessState.String())
+		if e.ctxErr != nil {
+			out += fmt.Sprintf("\n%s", e.ctxErr)
+		}
+	} else {
+		out = fmt.Sprintf("%q exited: %s", e.args, e.err.Error())
+		if e.ctxErr != nil {
+			out += fmt.Sprintf("\n%s", e.ctxErr)
+		}
+	}
+
+	if e.stderr != "" {
+		out += fmt.Sprintf("\nstderr: %q", e.stderr)
+	}
+
+	return out
+}
