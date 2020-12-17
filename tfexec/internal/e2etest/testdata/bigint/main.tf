16fc138b3663ac0e9d744d1b490bf0366ed31ea5
Katy Moe <katy@katy.moe>
2020-12-17 11:47:05 +0000

    add test for bigint json.Number representation

---
+++ tfexec/internal/e2etest/testdata/bigint/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    random = {
+      version = "3.0.0"
+    }
+  }
+}
+
+resource "random_integer" "bigint" {
+  max  = 7227701560655103598
+  min  = 7227701560655103597
+  seed = 12345
+}
