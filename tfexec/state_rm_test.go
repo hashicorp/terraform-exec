eb29e456adf2dbd9faca522c15f4877131b834f7
Aleksander Aleksic <aleksander@aleksic.no>
2021-01-20 13:20:28 +0100

    Implemented state remove command

---
+++ tfexec/state_rm_test.go
@@ -0,0 +1,56 @@
+package tfexec
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hashicorp/terraform-exec/tfexec/internal/testutil"
+)
+
+func TestStateRmCmd(t *testing.T) {
+	td := testTempDir(t)
+
+	tf, err := NewTerraform(td, tfVersion(t, testutil.Latest013))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// empty env, to avoid environ mismatch in testing
+	tf.SetEnv(map[string]string{})
+
+	t.Run("defaults", func(t *testing.T) {
+		stateRmCmd, err := tf.stateRmCmd(context.Background(), "testsource")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assertCmd(t, []string{
+			"state",
+			"rm",
+			"-no-color",
+			"-lock-timeout=0s",
+			"-lock=true",
+			"testsource",
+		}, nil, stateRmCmd)
+	})
+
+	t.Run("override all defaults", func(t *testing.T) {
+		stateRmCmd, err := tf.stateRmCmd(context.Background(), "testsrc", Backup("testbackup"), BackupOut("testbackupout"), LockTimeout("200s"), State("teststate"), StateOut("teststateout"), Lock(false))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assertCmd(t, []string{
+			"state",
+			"rm",
+			"-no-color",
+			"-backup=testbackup",
+			"-backup-out=testbackupout",
+			"-lock-timeout=200s",
+			"-state=teststate",
+			"-state-out=teststateout",
+			"-lock=false",
+			"testsrc",
+		}, nil, stateRmCmd)
+	})
+}
