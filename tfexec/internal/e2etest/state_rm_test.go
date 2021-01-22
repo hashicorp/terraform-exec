3892f6e4fd012ef5b77b9ed6cc619a7035f55585
Aleksander Aleksic <aleksander@aleksic.no>
2021-01-22 08:01:41 +0100

    Added e2e test for state remove command

---
+++ tfexec/internal/e2etest/state_rm_test.go
@@ -0,0 +1,45 @@
+package e2etest
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hashicorp/go-version"
+	tfjson "github.com/hashicorp/terraform-json"
+
+	"github.com/hashicorp/terraform-exec/tfexec"
+)
+
+func TestStateRm(t *testing.T) {
+	runTest(t, "basic_with_state", func(t *testing.T, tfv *version.Version, tf *tfexec.Terraform) {
+		if tfv.LessThan(providerAddressMinVersion) {
+			t.Skip("state file provider FQNs not compatible with this Terraform version")
+		}
+
+		err := tf.Init(context.Background())
+		if err != nil {
+			t.Fatalf("error running Init in test directory: %s", err)
+		}
+
+		err = tf.StateRm(context.Background(), "null_resource.foo")
+		if err != nil {
+			t.Fatalf("error running StateRm: %s", err)
+		}
+
+		// test that the new state is as expected
+		expected := &tfjson.State{
+			FormatVersion: "0.1",
+			// TerraformVersion is ignored to facilitate latest version testing
+			Values: nil,
+		}
+
+		actual, err := tf.Show(context.Background())
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if diff := diffState(expected, actual); diff != "" {
+			t.Fatalf("mismatch (-want +got):\n%s", diff)
+		}
+	})
+}
