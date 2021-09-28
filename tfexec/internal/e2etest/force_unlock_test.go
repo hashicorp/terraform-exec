fdc73894c6944aa49822a117da766b9cbc51623b
Alec Rabold <alecrabold@outlook.com>
2021-09-27 19:28:16 -0700

    add command for force-unlock

---
+++ tfexec/internal/e2etest/force_unlock_test.go
@@ -0,0 +1,43 @@
+package e2etest
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hashicorp/go-version"
+
+	"github.com/hashicorp/terraform-exec/tfexec"
+)
+
+// LockID set in the test fixture
+const inmemLockID = "2b6a6738-5dd5-50d6-c0ae-f6352977666b"
+
+var forceUnlockDirArgMaxVersion = version.Must(version.NewVersion("0.15.0"))
+
+func TestForceUnlock(t *testing.T) {
+	runTest(t, "inmem-backend-locked", func(t *testing.T, tfv *version.Version, tf *tfexec.Terraform) {
+		err := tf.Init(context.Background())
+		if err != nil {
+			t.Fatalf("error running Init: %v", err)
+		}
+
+		err = tf.ForceUnlock(context.Background(), inmemLockID)
+		if err != nil {
+			t.Fatalf("error running ForceUnlock: %v", err)
+		}
+	})
+	runTest(t, "inmem-backend-locked", func(t *testing.T, tfv *version.Version, tf *tfexec.Terraform) {
+		if tfv.GreaterThanOrEqual(forceUnlockDirArgMaxVersion) {
+			t.Skip("legacy positional path argument deprecated in favor of global -chdir flag")
+		}
+		err := tf.Init(context.Background())
+		if err != nil {
+			t.Fatalf("error running Init: %v", err)
+		}
+
+		err = tf.ForceUnlock(context.Background(), inmemLockID, tfexec.Dir(tf.WorkingDir()))
+		if err != nil {
+			t.Fatalf("error running ForceUnlock: %v", err)
+		}
+	})
+}
