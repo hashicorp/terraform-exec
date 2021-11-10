fa8cfa3525b99b7735534fb635f65d7979d8a912
Katy Moe <katy@katy.moe>
2021-11-10 12:36:09 +0000

    remove goreleaser

--- scripts/release/release.sh
+++ scripts/release/release.sh
@@ -84,15 +84,10 @@ function commitMain {
   printf "ok!\n"
 }
 
-function createGitHubRelease {
-    curl -sL https://git.io/goreleaser | bash
-}
-
 function main {
   init
   changelogMain
   commitMain
-  createGitHubRelease
 }
 
 main
