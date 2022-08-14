eb08bfb808a0ad668445bf616a206b85f8028eeb
Katy Moe <katy@katy.moe>
2022-08-14 15:08:29 +0200

    try newer versions

--- tfexec/workspace_show_test.go
+++ tfexec/workspace_show_test.go
@@ -2,39 +2,10 @@ package tfexec
 
 import (
 	"context"
-	"errors"
-	"runtime"
 	"testing"
 
 	"github.com/hashicorp/terraform-exec/tfexec/internal/testutil"
 )
-
-func TestWorkspaceShowCmd_v012(t *testing.T) {
-	if runtime.GOOS == "darwin" && runtime.GOARCH == "arm64" {
-		t.Skip("Terraform for darwin/arm64 is not available until v1")
-	}
-
-	td := t.TempDir()
-
-	tf, err := NewTerraform(td, tfVersion(t, "0.9.11"))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// empty env, to avoid environ mismatch in testing
-	tf.SetEnv(map[string]string{})
-
-	_, err = tf.workspaceShowCmd(context.Background())
-	if err == nil {
-		t.Fatal("expected old version to fail")
-	}
-
-	var expectedErr *ErrVersionMismatch
-	if !errors.As(err, &expectedErr) {
-		t.Fatalf("error doesn't match: %#v", err)
-	}
-
-}
 
 func TestWorkspaceShowCmd_v1(t *testing.T) {
 	td := t.TempDir()
