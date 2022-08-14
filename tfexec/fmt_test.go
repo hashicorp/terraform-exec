c7d07934409d4ab9c30f227aa96700cfe1cf9dde
Katy Moe <katy@katy.moe>
2022-08-14 15:08:29 +0200

    try newer versions

--- tfexec/fmt_test.go
+++ tfexec/fmt_test.go
@@ -2,19 +2,20 @@ package tfexec
 
 import (
 	"context"
-	"errors"
 	"runtime"
 	"testing"
+
+	"github.com/hashicorp/terraform-exec/tfexec/internal/testutil"
 )
 
-func TestFormat(t *testing.T) {
+func TestFormatCmd(t *testing.T) {
 	if runtime.GOOS == "darwin" && runtime.GOARCH == "arm64" {
 		t.Skip("Terraform for darwin/arm64 is not available until v1")
 	}
 
 	td := t.TempDir()
 
-	tf, err := NewTerraform(td, tfVersion(t, "0.7.6"))
+	tf, err := NewTerraform(td, tfVersion(t, testutil.Latest_v1_1))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -22,15 +23,34 @@ func TestFormat(t *testing.T) {
 	// empty env, to avoid environ mismatch in testing
 	tf.SetEnv(map[string]string{})
 
-	t.Run("too old version", func(t *testing.T) {
-		_, err := tf.formatCmd(context.Background(), []string{})
-		if err == nil {
-			t.Fatal("expected old version to fail")
+	t.Run("defaults", func(t *testing.T) {
+		fmtCmd, err := tf.formatCmd(context.Background(), []string{})
+		if err != nil {
+			t.Fatal(err)
 		}
 
-		var expectedErr *ErrVersionMismatch
-		if !errors.As(err, &expectedErr) {
-			t.Fatalf("error doesn't match: %#v", err)
+		assertCmd(t, []string{
+			"fmt",
+			"-no-color",
+		}, nil, fmtCmd)
+	})
+
+	t.Run("override all defaults", func(t *testing.T) {
+		fmtCmd, err := tf.formatCmd(context.Background(),
+			[]string{"string1", "string2"},
+			Recursive(true),
+			Dir("mydir"))
+		if err != nil {
+			t.Fatal(err)
 		}
+
+		assertCmd(t, []string{
+			"fmt",
+			"-no-color",
+			"string1",
+			"string2",
+			"-recursive",
+			"mydir",
+		}, nil, fmtCmd)
 	})
 }
