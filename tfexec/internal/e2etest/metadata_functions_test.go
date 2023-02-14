bb68e3a495186ecb751447ea4a84a67872a5421a
Daniel Banck <daniel@dbanck.de>
2023-02-14 18:04:44 +0100

    Add e2e test

---
+++ tfexec/internal/e2etest/metadata_functions_test.go
@@ -0,0 +1,23 @@
+package e2etest
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hashicorp/go-version"
+
+	"github.com/hashicorp/terraform-exec/tfexec"
+)
+
+func TestMetadataFunctions(t *testing.T) {
+	runTest(t, "basic", func(t *testing.T, tfv *version.Version, tf *tfexec.Terraform) {
+		if tfv.LessThan(metadataFunctionsMinVersion) {
+			t.Skip("metadata functions command is not available in this Terraform version")
+		}
+
+		_, err := tf.MetadataFunctions(context.Background())
+		if err != nil {
+			t.Fatalf("error running MetadataFunctions: %s", err)
+		}
+	})
+}
