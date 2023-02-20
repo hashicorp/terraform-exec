8daefd30cb235f24f00dccadfa6e38ac619b0ee6
Radek Simko <radek.simko@gmail.com>
2023-02-20 15:55:30 +0000

    experiment workflow

--- scripts/release/release.sh
+++ scripts/release/release.sh
@@ -23,6 +23,8 @@ function init {
   if [ "$CI" = true ] ; then
     git config --global user.email "proj-terraform-exec@hashicorp.com"
     git config --global user.name "terraform-exec [bot]"
+    git config --global commit.gpgsign true
+    git config --global user.signingkey "${SIGNORE_SIGNER}"
     git config --global gpg.program scripts/release/signore-wrapper.sh
   fi
 
@@ -60,20 +62,27 @@ function modifyVersionFiles {
 }
 
 function commitChanges {
+  bash --version
+  signore version
+
   git add CHANGELOG.md
   modifyVersionFiles
   git add internal/version/version.go
 
   if [ "$CI" = true ] ; then
-      git commit -m "v${TARGET_VERSION} [skip ci]" --gpg-sign="${SIGNORE_SIGNER}"
-      git tag -a -m "v${TARGET_VERSION}" -s -u "${SIGNORE_SIGNER}" "v${TARGET_VERSION}"
+      git commit -m "v${TARGET_VERSION} [skip ci]"
+      # git tag -a -m "v${TARGET_VERSION}" -s "v${TARGET_VERSION}"
   else
       git commit -m "v${TARGET_VERSION} [skip ci]"
       git tag -a -m "v${TARGET_VERSION}" -s "v${TARGET_VERSION}"
   fi
 
-  git push origin "${GITHUB_REF_NAME}"
-  git push origin "v${TARGET_VERSION}"
+  echo "---"
+  git log -1
+  echo "---"
+
+  #git push origin "${GITHUB_REF_NAME}"
+  #git push origin "v${TARGET_VERSION}"
 }
 
 function commitMain {
