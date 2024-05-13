f40d1d562ac67b1ce34573bb45eede34ab75d1df
Junfeng Wu <wjf3121@hotmail.com>
2024-05-13 14:04:51 +0800

    feat: add graceful shutdown option for apply&destroy (#2)

--- tfexec/cmd_unix.go
+++ tfexec/cmd_unix.go
@@ -1,16 +1,13 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
 
-//go:build linux
-// +build linux
+//go:build unix && !linux
 
 package tfexec
 
 import "syscall"
 
 var defaultSysProcAttr = &syscall.SysProcAttr{
-	// kill children if parent is dead
-	Pdeathsig: syscall.SIGKILL,
 	// set process group ID
 	Setpgid: true,
 }
