ce9c76020c3eac27d6f998bc8288f8cb4591a902
Austin Valle <austinvalle@gmail.com>
2026-01-06 18:41:30 -0500

    tfexec: Add provided reattach support to all `terraform workspace` subcommands

--- tfexec/workspace_list_test.go
+++ tfexec/workspace_list_test.go
@@ -4,10 +4,60 @@
 package tfexec
 
 import (
+	"context"
 	"fmt"
 	"reflect"
 	"testing"
+
+	"github.com/hashicorp/terraform-exec/tfexec/internal/testutil"
 )
+
+func TestWorkspaceListCmd(t *testing.T) {
+	tf, err := NewTerraform(t.TempDir(), tfVersion(t, testutil.Latest_v1))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// empty env, to avoid environ mismatch in testing
+	tf.SetEnv(map[string]string{})
+
+	t.Run("defaults", func(t *testing.T) {
+		workspaceListCmd, err := tf.workspaceListCmd(context.Background())
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assertCmd(t, []string{
+			"workspace", "list",
+			"-no-color",
+		}, nil, workspaceListCmd)
+	})
+
+	t.Run("reattach config", func(t *testing.T) {
+		workspaceListCmd, err := tf.workspaceListCmd(context.Background(), Reattach(map[string]ReattachConfig{
+			"registry.terraform.io/hashicorp/examplecloud": {
+				Protocol:        "grpc",
+				ProtocolVersion: 6,
+				Pid:             1234,
+				Test:            true,
+				Addr: ReattachConfigAddr{
+					Network: "unix",
+					String:  "/fake_folder/T/plugin123",
+				},
+			},
+		}))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assertCmd(t, []string{
+			"workspace", "list",
+			"-no-color",
+		}, map[string]string{
+			"TF_REATTACH_PROVIDERS": `{"registry.terraform.io/hashicorp/examplecloud":{"Protocol":"grpc","ProtocolVersion":6,"Pid":1234,"Test":true,"Addr":{"Network":"unix","String":"/fake_folder/T/plugin123"}}}`,
+		}, workspaceListCmd)
+	})
+}
 
 func TestParseWorkspaceList(t *testing.T) {
 	for i, c := range []struct {
