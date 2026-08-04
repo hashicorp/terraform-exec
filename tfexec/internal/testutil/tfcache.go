52c8030cd6a75cf1a1d782d9dc8e6dc43629c47a
Sarah French <sarah.french@hashicorp.com>
2026-08-04 13:37:49 +0100

    fix: Update latest v1.15 release

--- tfexec/internal/testutil/tfcache.go
+++ tfexec/internal/testutil/tfcache.go
@@ -38,7 +38,7 @@ const (
 	Latest_v1_11       = "1.11.4"
 	Latest_v1_12       = "1.12.2"
 	Latest_Alpha_v1_14 = "1.14.0-alpha20250903"
-	Latest_v1_15       = "1.15.1"
+	Latest_v1_15       = "1.15.8"
 	Latest_v1_16       = "1.16.0"
 
 	Latest_v1 = "1.16.1"
